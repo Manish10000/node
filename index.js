dddac5b4f37270006bf92d7a34fe42c9971280c5
Manish10000 <135357928+Manish10000@users.noreply.github.com>
2024-01-12 10:56:43 +0530

    2nd node

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express=require('express')
 const app=express();
+const cors = require('cors')
+
 app.use(express.json());
 const candidateRoute=require('./routes/candidateRoute')
+app.use(cors())
 
 app.get('/api/v1',(req,res)=>{
     res.send("<h1>This is home page </h1>")
